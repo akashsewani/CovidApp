5cf9e77a1c901d3eef603e6e2c79da1171a6f4f4
akashsewani <akashsewani44@gmail.com>
2020-07-14 15:31:07 +0000

    Python app initialized and added gitignore file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from pymongo import MongoClient
+
+app = Flask(__name__)
+api = Api(app)
+
+class DefaultApi(Resource):
+    def get(self):
+        return "Default Api Get Called"
+    def post(self):
+        data = request.get_json()
+        return jsonify({'data': data})
+
+api.add_resource(DefaultApi, '/')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+

b9a0cae8b9c7502a013b3ec99e3bb17ecb218335
akashsewani <akashsewani44@gmail.com>
2020-07-18 18:19:42 +0000

    changes to app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+from bson.json_util import dumps
+
 
 app = Flask(__name__)
 api = Api(app)
+conn = MongoClient()
+db = conn.Bookstore
+#or db = pymongo.MongoClient.Bookstore
+db.Books.insert_one({"Name":"Book2"})
+db.Books.insert_one({"Name":"Book3"})
+
+cursor = db.Books.find({})
+list_cursor = list(cursor)
+json_data = dumps(list_cursor, indent=2)
+print(json_data)
+
 
 class DefaultApi(Resource):
     def get(self):

fd10d8a116df08d5f27456f9e900bb8caa47a34a
akashsewani <akashsewani44@gmail.com>
2020-07-19 10:46:11 +0000

    Added database files, and app.py api changes

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.json_util import dumps
 
-
 app = Flask(__name__)
 api = Api(app)
 conn = MongoClient()
 db = conn.covidapp
-#or db = pymongo.MongoClient.Bookstore
+
 @app.route('/api/v1/resources/dailycases/all', methods = ['GET'])
 @app.route('/', methods=['GET'])
 
@@ -20,14 +19,20 @@ def DateWiseData_all():
 
 
 
-
-@app.route('/state')
+@app.route('/allstates')
 def StateWiseData_all():
     cursor = db.StatewiseCaseInfo.find({})
     list_cursor = list(cursor)
     json_data =dumps(list_cursor, indent=2)
     return json_data
 
+@app.route('/state')
+def StateWiseData():
+    sc = request.args['sc']
+    cursor = db.StatewiseCaseInfo.find({"statecode":sc})
+    list_cursor = list(cursor)
+    json_data =dumps(list_cursor, indent=2)
+    return json_data
 
 if __name__ == '__main__':
     app.run(debug=True) 
